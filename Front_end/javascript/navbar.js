ebb89276831a5a7f16decfd13a4c2cbf10dcf68b
Juntao <jyuu0096@student.monash.edu>
2023-08-30 13:25:16 +1000

    Formattied code and added comments

--- Front_end/javascript/navbar.js
+++ Front_end/javascript/navbar.js
@@ -1,23 +1,25 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-})
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = function() {
-    sections.forEach(function(sec) {
-        let top = window.scrollY;
-        let offset = sec.offsetTop -500;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+window.onscroll = function () {
+  sections.forEach(function (sec) {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 500;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(function(links) {
-                links.classList.remove('active');
-            });
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(function (links) {
+        links.classList.remove("active");
+      });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
 };
