de1b5695677d3b475bd742a9264a2f40c1676527
Juntao <jyuu0096@student.monash.edu>
2023-08-31 18:15:44 +1000

    fixed file name and added some data

--- Front_end/javascript/map.js
+++ Front_end/javascript/map.js
@@ -1,3 +1,5 @@
+let state_name;
+
 document.querySelectorAll(".paths").forEach((path) => {
   path.addEventListener("mouseover", function (e) {
     const divElement = document.getElementById("aus-map"); // Replace with your div's ID
@@ -16,5 +18,58 @@ document.querySelectorAll(".paths").forEach((path) => {
   path.addEventListener("mouseleave", function () {
     document.getElementById("map-tip").style.opacity = 0;
   });
-});
+
+  path.addEventListener("click", function () {
+    state_name = path.id;
+    console.log(state_name);
+    chartIt();
+  });
+  });
+
+
+async function chartIt(){
+    const ausData = await getData();
+    const ctx = document.getElementById('chart1');
+    new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ausData.xs,
+        datasets: [{
+        label: state_name,
+        data: ausData.ys,
+        borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+    });
+
+  }
+
+async function getData(){
+    
+    const xs = [];
+    const ys = [];
+
+    const aus_url='../json/aus_data.json'
+    const response = await fetch(aus_url);
+    const aus_data = await response.json();
+
+    const regionName = state_name; // Replace with the region name you want to plot
+    const regionData = aus_data.filter(entry => entry.region === regionName);
+
+    const financialYears = regionData.map(entry => entry["financial year"]);
+    xs.push(...financialYears);
+
+    const electricityUsage = regionData.map(entry => entry["electricity_usage"]);
+    ys.push(...electricityUsage);
+
+    return {xs, ys};
+}
+
 
