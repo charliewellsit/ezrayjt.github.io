fc3c7a1a501748e84f11045dd8430a066a0b40a7
lleytonj5 <86594328+lleytonj5@users.noreply.github.com>
2023-09-01 11:15:18 +1000

    Remove region_id from select statement
    It's not used in the output

--- Backend/azure_function/function_app.py
+++ Backend/azure_function/function_app.py
@@ -51,7 +51,7 @@ def get_data(req: func.HttpRequest) -> func.HttpResponse:
     )
 
     # SQL query to get data from the database
-    query = "SELECT r.region_id, r.region_name, c.financial_start_year, c.electricity_usage, c.gas_usage, g.non_renewable_electricity_total, g.renewable_electricity_total, g.total_electricity_generation, g.total_gas_generation FROM regions r LEFT JOIN energy_consumption c ON r.region_id = c.region_id RIGHT JOIN energy_generation g ON r.region_id = g.region_id AND c.financial_start_year = g.financial_start_year"
+    query = "SELECT r.region_name, c.financial_start_year, c.electricity_usage, c.gas_usage, g.non_renewable_electricity_total, g.renewable_electricity_total, g.total_electricity_generation, g.total_gas_generation FROM regions r LEFT JOIN energy_consumption c ON r.region_id = c.region_id RIGHT JOIN energy_generation g ON r.region_id = g.region_id AND c.financial_start_year = g.financial_start_year"
 
     # Execute the query using the DatabaseManager
     result = db_manager.execute_query(query)
@@ -59,14 +59,14 @@ def get_data(req: func.HttpRequest) -> func.HttpResponse:
     # Process the query result and format it as JSON
     data = []
     for each in result:
-        region = each[1]
-        year = each[2] + 1
-        electricity_usage = round(each[3])
-        gas_usage = round(each[4])
-        elect_non_renewable_generated = round(each[5])
-        elect_renewable_generated = round(each[6])
-        total_elect_generated = round(each[7])
-        total_gas_generated = round(each[8])
+        region = each[0]
+        year = each[1] + 1
+        electricity_usage = round(each[2])
+        gas_usage = round(each[3])
+        elect_non_renewable_generated = round(each[4])
+        elect_renewable_generated = round(each[5])
+        total_elect_generated = round(each[6])
+        total_gas_generated = round(each[7])
 
         data.append({
             'region': region,
