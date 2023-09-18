eb8c49cf81c14a0100c2d5763656d7501ca7f5e5
lleytonj5 <86594328+lleytonj5@users.noreply.github.com>
2023-09-18 17:55:41 +1000

    Add iteration 2 data retrieval methods to Azure Function

--- Backend/azure_function/function_app.py
+++ Backend/azure_function/function_app.py
@@ -43,7 +43,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def stayinalive(timer: func.TimerRequest):
     requests.get("https://ta21-2023-s2.azurewebsites.net/api/get_data")
 
-@app.route(route="get_data")
+@app.route()
 def get_data(req: func.HttpRequest):
     db_manager = get_database_manager("energy")
 
@@ -68,6 +68,133 @@ def get_data(req: func.HttpRequest):
     db_manager.close()
     result_json = json.dumps(data)
     
+    return func.HttpResponse(
+      result_json,
+      mimetype="application/json"
+    )
+
+# Route to get incandescent light bulb data for iteration 2
+@app.route('get_incandescent', methods=['GET'])
+def get_data_incandescent(req: func.HttpRequest):
+    db_manager_iteration2 = get_database_manager("iteration2")
+
+    # SQL query to get data from the database
+    query = "SELECT i.brand, i.model_number, i.lamp_power_watts, ROUND(i.lamp_light_lumens), i.lamp_life_hours, GROUP_CONCAT(m.manufacturer_country) AS country_list, ROUND(i.lamp_light_lumens / i.lamp_power_watts) AS efficiency FROM incandescents i JOIN incandescent_manufacturing m ON i.ID = m.incandescent_ID GROUP BY i.brand, i.model_number, i.lamp_power_Watts, i.lamp_light_lumens, i.lamp_life_hours"
+
+    # Execute the query using the DatabaseManager
+    result = db_manager_iteration2.execute_query(query)
+
+    # Process the query result and format it as JSON
+    data = []
+    for each in result:
+        brand = each[0].title()
+        model_number = each[1]
+        lamp_power_watts = each[2]
+        lamp_light_lumens = each[3]
+        lamp_life_hours = each[4]
+        country_list = each[5]
+        efficiency = each[6]
+
+        data.append({
+            'brand': brand,
+            'model': model_number,
+            'power': lamp_power_watts,
+            'brightness': lamp_light_lumens,
+            'life': lamp_life_hours,
+            'manufacturer_country': country_list,
+            # no unit for efficiency so can sort based on the efficiency for the frontend
+            'efficiency': efficiency
+        })
+
+    db_manager_iteration2.close()
+    result_json = json.dumps(data)
+    return func.HttpResponse(
+      result_json,
+      mimetype="application/json"
+    )
+
+# Route to get CFL data for iteration 2
+@app.route('get_CFL', methods=['GET'])
+def get_data_CFL(req: func.HttpRequest):
+    db_manager_iteration2 = get_database_manager("iteration2")
+
+    # SQL query to get data from the database
+    query = "SELECT brand, model_number, manufacturer_country, lamp_power_watts, ROUND(lamp_light_lumens), colour_temperature, lamp_life_hours, ROUND(lamp_light_lumens / lamp_power_watts) FROM CFLs"
+
+    # Execute the query using the DatabaseManager
+    result = db_manager_iteration2.execute_query(query)
+
+    # Process the query result and format it as JSON
+    data = []
+    for each in result:
+        brand = each[0].title()
+        model_number = each[1]
+        manufacturer_country = each[2]
+        lamp_power_watts = each[3]
+        lamp_light_lumens = each[4]
+        colour_temperature = each[5]
+        lamp_life_hours = each[6]
+        efficiency = each[7]
+
+        data.append({
+            'brand': brand,
+            'model': model_number,
+            'power': lamp_power_watts,
+            'brightness': lamp_light_lumens,
+            'life': lamp_life_hours,
+            'manufacturer_country': manufacturer_country,
+            'colour_temperature': colour_temperature,
+            # no unit for efficiency so can sort based on the efficiency for the frontend
+            'efficiency': efficiency
+        })
+
+    db_manager_iteration2.close()
+    result_json = json.dumps(data)
+    return func.HttpResponse(
+      result_json,
+      mimetype="application/json"
+    )
+
+# Route to get waste management facilities data for iteration 2
+@app.route('get_waste_mgmt_facilities', methods=['GET'])
+def get_data_waste_facilities(req: func.HttpRequest):
+    db_manager_iteration2 = get_database_manager("iteration2")
+
+    # SQL query to get data from the database
+    query = "SELECT x, y, facility_management_type, facility_infrastructure_type, facility_owner, facility_name, state_or_territory, address, suburb, postcode FROM waste_management_facilities"
+
+    # Execute the query using the DatabaseManager
+    result = db_manager_iteration2.execute_query(query)
+
+    # Process the query result and format it as JSON
+    data = []
+    for each in result:
+        x = each[0]
+        y = each[1]
+        facility_management_type = each[2].title()
+        facility_infrastructure_type = each[3].title()
+        facility_owner = each[4].title()
+        facility_name = each[5].title()
+        state_or_territory = each[6]
+        address = each[7].title()
+        suburb = each[8].title()
+        postcode = each[9]
+
+        data.append({
+            'x-coordinate': x,
+            'y-coordinate': y,
+            'type': facility_management_type,
+            'sub-type': facility_infrastructure_type,
+            'owner': facility_owner,
+            'name': facility_name,
+            'state': state_or_territory,
+            'address': address,
+            'suburb': suburb,
+            'postcode': postcode
+        })
+
+    db_manager_iteration2.close()
+    result_json = json.dumps(data)
     return func.HttpResponse(
       result_json,
       mimetype="application/json"
