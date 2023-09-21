e0a4fe269a4534c0bb000480f7de351460d88ab7
Sophia Amanda <sama0003@student.monash.edu>
2023-09-21 14:58:25 +1000

    fixed navbar

--- iteration2/Front_end/javascript/navbar.js
+++ iteration2/Front_end/javascript/navbar.js
@@ -1,50 +1,31 @@
-// window.addEventListener("scroll", function () {
-//   let header = document.querySelector("header");
-//   header.classList.toggle("sticky", window.scrollY > 0);
-// });
-
-// let sections = document.querySelectorAll("section");
-// let navLinks = document.querySelectorAll("header nav a");
-
-// window.onscroll = function () {
-//   sections.forEach(function (sec) {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 150;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute("id");
-
-//     if (top >= offset && top < offset + height) {
-//       navLinks.forEach(function (links) {
-//         links.classList.remove("active");
-//       });
-//       document
-//         .querySelector("header nav a[href*=" + id + "]")
-//         .classList.add("active");
-//     }
-//   });
-// };
-
-// Function to check if the current page is "lights.html" or "cfl.html or other html pages that are not index.html"
-function isLightsOrCFLPage() {
-  const currentPage = window.location.pathname;
-  return currentPage.includes("lights.html") || currentPage.includes("cfl.html") || currentPage.includes("incandescents.html") || currentPage.includes("waste.html") || currentPage.includes(buildingWaste.html) || currentPage.includes(HouseholdCleaners.html) || currentPage.includes(LightingContaining.html) || currentPage.includes(Paint.html);
-}
-
 // Function to add the "active" class to the "Services" link
-function highlightServicesLink() {
-  const servicesNavLink = document.querySelector("header nav a[href='index.html#Services']");
+function highlightNavSection() {
+  const currentPage = window.location.pathname;
+  const lightPages = currentPage.includes("lights.html") || currentPage.includes("lightbulb.html") || currentPage.includes("cfl.html") || currentPage.includes("incandescents.html")
+  const wastePages = currentPage.includes("buildingWaste.html") || currentPage.includes("HouseholdCleaners.html") || currentPage.includes("LightingContaining.html") || currentPage.includes("Paint.html")|| currentPage.includes("waste.html");
+  const lightsNavLink = document.querySelector("header nav a[href='lights.html']");
+  const wasteNavLink = document.querySelector("header nav a[href='waste.html']");
   const homeNavLink = document.querySelector("header nav a[href='index.html#home']");
-  if (servicesNavLink) {
-    servicesNavLink.classList.add("active");
+  if (lightPages) {
+    lightsNavLink.classList.add("active");
+    wasteNavLink.classList.remove("active");
     homeNavLink.classList.remove("active");
+  }
+  else if (wastePages){
+    lightsNavLink.classList.remove("active");
+    wasteNavLink.classList.add("active");
+    homeNavLink.classList.remove("active");
+  }
+  else {
+    lightsNavLink.classList.remove("active");
+    wasteNavLink.classList.remove("active");
+    homeNavLink.classList.add("active");
   }
 }
 
 // Call the highlightServicesLink function on page load
 window.addEventListener("load", function () {
-  if (isLightsOrCFLPage()) {
-    highlightServicesLink();
-  }
+  highlightNavSection();
 });
 
 // Your existing code for scrolling-based highlighting
