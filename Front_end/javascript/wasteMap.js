848271c32f59702e9a1e50fdfc6e4cdfa8769d33
Juntao <jyuu0096@student.monash.edu>
2023-09-30 23:13:43 +1000

    updated feature 4 to show the facility info

--- Front_end/javascript/wasteMap.js
+++ Front_end/javascript/wasteMap.js
@@ -1,67 +1,146 @@
-const wastemap = L.map('wasteMap'); 
-// Initializes map
+//get data from json file
+let waste_data;
+const waste_url='Front_end/json/waste.json';
 
-wastemap.setView([-37.8136, 144.9631], 9); 
-// Sets initial coordinates and zoom level
+async function getData(){
+    const response = await fetch(waste_url);
+    waste_data = await response.json();
+  }
+
+getData();
+
+
+
+
+
+
+
+//initialise map
+let myMap = L.map('wasteMap');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(wastemap); 
-// Sets map data source and associates with map
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
 
-do {
-    latOffset = (Math.random() * 0.22) - 0.11;
-    lngOffset = (Math.random() * 0.22) - 0.11; 
-  } while (Math.abs(latOffset) >= 0.4 || Math.abs(lngOffset) >= 0.4);
+myMap.setView([-37.8136, 144.9631], 9); 
 
-let marker, circle, zoomed;
 
-navigator.geolocation.watchPosition(success, error);
 
-function success(pos) {
 
-    const lat = pos.coords.latitude;
-    const lng = pos.coords.longitude;
 
-    if (marker) {
-        wastemap.removeLayer(marker);
-        wastemap.removeLayer(circle);
+
+//get user location and add markers
+function getLocation() {
+
+    let success = (position) => {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+        let marker = L.marker([lat, lng]).addTo(myMap);
+
+        let userLocation = {
+          latitude: lat,
+          longitude: lng,
+        };
+        const nearestFasility = findNearestCoordinate(userLocation, waste_data);
+
+        let circle = L.circle([nearestFasility.latitude, nearestFasility.longitude], {
+          radius: 100,
+          color: 'green',
+          fillColor: 'green',
+          fillOpacity: 0.7
+        }).addTo(myMap);
+
+        marker.bindPopup("This is your location.").openPopup();
+
+        circle.bindPopup(`
+        <p><strong>Name:</strong> ${nearestFasility.name}</p>
+        <p><strong>Type:</strong> ${nearestFasility.type}</p>
+        <p><strong>Sub-type:</strong> ${nearestFasility['sub-type']}</p>
+        <p><strong>Owner:</strong> ${nearestFasility.owner}</p>
+        <p><strong>State:</strong> ${nearestFasility.state}</p>
+        <p><strong>Address:</strong> ${nearestFasility.address}, ${nearestFasility.suburb}, ${nearestFasility.postcode}</p>
+        <hr>
+    `);
+
+        // set map view to show both marker and circle
+        let bounds = [marker.getLatLng(), circle.getBounds().getNorthEast(), circle.getBounds().getSouthWest()];
+        myMap.fitBounds(bounds);
     }
-    // Removes any existing marker and circule (new ones about to be set)
 
-    marker = L.marker([lat, lng]).addTo(wastemap);
-    circle = L.circle([lat+latOffset, lng+lngOffset], { 
-        radius: 700,
-        color: 'green',
-        fillColor: 'green',
-        fillOpacity: 0.7
-    }).addTo(wastemap);
-    // circle2 = L.circle([lat, lng], { 
-    //     radius: 17000,
-    //     fillOpacity: 0.1
-    // }).addTo(wastemap);
-    marker.bindPopup("It's your location").openPopup();
-    circle.bindPopup("Nearest Waste Recycling Facility.");
-    // Adds marker to the map and a circle for accuracy
-
-
-    if (!zoomed) {
-        zoomed = wastemap.fitBounds(circle.getBounds()); 
+    let error = () => {
+        alert("Unable to retrieve your location");
     }
-    // Set zoom to boundaries of accuracy circle
 
-    wastemap.setView([lat, lng], 10);
-    // Set map focus to current user position
-
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 
-function error(err) {
+getLocation();
 
-    if (err.code === 1) {
-        alert("Please allow geolocation access");
-    } else {
-        alert("Cannot get current location");
+
+
+
+
+
+
+
+
+// get nearest Fasility
+function degToRad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    // console.log("lat1:", lat1, "lon1:", lon1, "lat2:", lat2, "lon2:", lon2);
+
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = degToRad(lat2 - lat1);
+    const dLon = degToRad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance in kilometers
+    // console.log("Distance:", distance);
+    return distance;
+  }
+  
+  
+  function findNearestCoordinate(currentLocation, coordinates) {
+    let minDistance = Number.MAX_VALUE;
+    let nearestCoordinate = null;
+
+  
+    for (const coordinate of coordinates) {
+
+      const distance = getDistance(
+        currentLocation.latitude,
+        currentLocation.longitude,
+        coordinate.latitude,
+        coordinate.longitude
+      );
+
+  
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestCoordinate = coordinate;
+      }
     }
+  
+    return nearestCoordinate;
+  }
+  
 
-}
+  // Example usage:
+  const currentLocation = {
+    latitude: 37.7749,
+    longitude: -122.4194,
+  };
+  
+  const coordinateList = [
+    { latitude: 37.7749, longitude: -122.4194 },
+    { latitude: 34.0522, longitude: -118.2437 },
+  ];
+  
+  // const nearestCoordinate = findNearestCoordinate(currentLocation, coordinateList);
+  // console.log("Nearest Coordinate:", nearestCoordinate);
+  
