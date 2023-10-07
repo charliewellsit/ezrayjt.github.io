7040ef6b12c8bb26db285cb4ad85d5f91be4e0a8
Sophia Amanda <sama0003@student.monash.edu>
2023-10-08 01:33:42 +1100

    updated until data vis 2 - need to complete 'AC' parts and fix UI/UX

--- Backend/api_handler_iteration3.py
+++ Backend/api_handler_iteration3.py
@@ -16,39 +16,15 @@ db_manager_iteration3 = DatabaseManager(
     database="iteration3"
 )
 
-# Route to get refrigerators data for iteration 2
+# Route to get refrigerators data for iteration 3
 @app.route('/api/get_refrigerators', methods=['GET'])
 def get_data_refrigerators():
     # SQL query to get data from the database
     # query = "SELECT brand, model_number, is_refrigerator, is_freezer, energy_usage_kwh_per_month, total_volume_litres, star_rating, CASE WHEN total_volume_litres <= 200 THEN 'Small' WHEN total_volume_litres <= 350 THEN 'Medium' WHEN total_volume_litres <= 500 THEN 'Large' ELSE 'Extra Large' END AS volume_category FROM Refrigerators"
     query = "SELECT brand, CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END AS 'type', ROUND(AVG(energy_usage_kwh_per_month), 3) AS average_energy_consumption, star_rating, CASE WHEN total_volume_litres <= 200 THEN 'Small' WHEN total_volume_litres <= 350 THEN 'Medium' WHEN total_volume_litres <= 500 THEN 'Large' ELSE 'Extra Large' END AS volume_category FROM Refrigerators GROUP BY brand, type, volume_category, star_rating ORDER BY count(*) DESC, brand, type, star_rating, volume_category"
 
-
     # Execute the query using the DatabaseManager
     result = db_manager_iteration3.execute_query(query)
-
-    # # Process the query result and format it as JSON
-    # data = []
-    # for each in result:
-    #     brand = each[0].title()
-    #     model_number = each[1]
-    #     is_refrigerator = each[2]
-    #     is_freezer = each[3]
-    #     energy_usage_kwh_per_month = float(each[4])
-    #     total_volume_litres = each[5]
-    #     star_rating = float(each[6])
-    #     volume_category = each[7]
-
-    #     data.append({
-    #         'brand': brand,
-    #         'model': model_number,
-    #         'is_refrigerator': is_refrigerator,
-    #         'is_freezer': is_freezer,
-    #         'energy_usage_kwh_per_month' : energy_usage_kwh_per_month,
-    #         'total_volume_litres': total_volume_litres,
-    #         'star_rating': star_rating,
-    #         'volume_category': volume_category
-    #     })
 
     # Process the query result and format it as JSON
     data = []
@@ -70,7 +46,63 @@ def get_data_refrigerators():
     result_json = json.dumps(data)
     return result_json
 
-# Route to get air conditioners data for iteration 2
+# Route to get refrigerators data for iteration 3
+@app.route('/api/get_fridge_avg_consumption', methods=['GET'])
+def get_data_fridge_avg_consumption():
+    # SQL query to get data from the database
+
+    # with volume category
+    # query = "SELECT CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END AS 'type', ROUND(AVG(energy_usage_kwh_per_month), 3) AS average_energy_consumption, star_rating, CASE WHEN total_volume_litres <= 200 THEN 'Small' WHEN total_volume_litres <= 350 THEN 'Medium' WHEN total_volume_litres <= 500 THEN 'Large' ELSE 'Extra Large' END AS volume_category FROM Refrigerators GROUP BY type, volume_category, star_rating ORDER BY type, star_rating, volume_category"
+
+    # with type
+    query = "SELECT CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END AS 'type', ROUND(AVG(energy_usage_kwh_per_month), 3) AS average_energy_consumption, star_rating FROM Refrigerators GROUP BY type, star_rating ORDER BY type, star_rating"
+
+    # Execute the query using the DatabaseManager
+    result = db_manager_iteration3.execute_query(query)
+
+    # Process the query result and format it as JSON
+    data = []
+    for each in result:
+        type = each[0].title()
+        average_energy_consumption = float(each[1])
+        star_rating = float(each[2])
+
+        data.append({
+            'type': type,
+            'Star Rating': star_rating,
+            'Average Energy Consumption (kW)' : average_energy_consumption,
+        })
+
+    result_json = json.dumps(data)
+    return result_json
+
+# Route to get refrigerators data for iteration 3
+@app.route('/api/get_fridge_highest_rating_consumption', methods=['GET'])
+def get_fridge_highest_rating_consumption():
+    # SQL query to get data from the database
+
+    query = "WITH RankedRefrigerators AS (SELECT CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END AS 'type', ROUND(AVG(energy_usage_kwh_per_month), 3) AS average_energy_consumption, star_rating, ROW_NUMBER() OVER (PARTITION BY CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END ORDER BY star_rating DESC) AS RowNum FROM Refrigerators GROUP BY CASE WHEN is_refrigerator = 1 AND is_freezer = 1 THEN 'Fridge&Freezer' WHEN is_refrigerator = 1 AND is_freezer = 0 THEN 'Fridge' ELSE 'Freezer' END, star_rating) SELECT type, average_energy_consumption, star_rating FROM RankedRefrigerators WHERE RowNum = 1 ORDER BY type"
+
+    # Execute the query using the DatabaseManager
+    result = db_manager_iteration3.execute_query(query)
+
+    # Process the query result and format it as JSON
+    data = []
+    for each in result:
+        type = each[0].title()
+        average_energy_consumption = float(each[1])
+        star_rating = float(each[2])
+
+        data.append({
+            'type': type,
+            'Star Rating': star_rating,
+            'Average Energy Consumption (kW)' : average_energy_consumption,
+        })
+
+    result_json = json.dumps(data)
+    return result_json
+
+# Route to get air conditioners data for iteration 3
 @app.route('/api/get_ac', methods=['GET'])
 def get_data_ac():
     # SQL query to get data from the database
--- Iteration3/Front_end/javascript/appliances.js
+++ Iteration3/Front_end/javascript/appliances.js
@@ -1,8 +1,68 @@
+async function FetchAppliancesAPI(){
+    let selectedType = document.getElementById('appliance').value;
+
+    if (selectedType === 'AC'){
+        return fetchAcDataFromAPI();
+    }
+    else if (selectedType === 'Fridge'){
+        return fetchFridgeDataFromAPI();
+    }
+}
+
 // Function to fetch data from the API
-async function fetchDataFromAPI() {
+async function fetchAcDataFromAPI() {
+    try {
+        const response = await fetch('http://127.0.0.1:5000/api/get_AC');
+        // const response = await fetch('https://ta21-2023-s2.azurewebsites.net/api/get_AC')
+        if (!response.ok) {
+            throw new Error('API request failed');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+
+// Function to fetch data from the API
+async function fetchFridgeDataFromAPI() {
     try {
         const response = await fetch('http://127.0.0.1:5000/api/get_refrigerators');
         // const response = await fetch('https://ta21-2023-s2.azurewebsites.net/api/get_refrigerators')
+        if (!response.ok) {
+            throw new Error('API request failed');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+// Function to fetch data from the API
+async function fetchFridgeConsumptionFromAPI() {
+    try {
+        const response = await fetch('http://127.0.0.1:5000/api/get_fridge_avg_consumption');
+        // const response = await fetch('https://ta21-2023-s2.azurewebsites.net/api/get_fridge_avg_consumption')
+        if (!response.ok) {
+            throw new Error('API request failed');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+// Function to fetch data from the API
+async function fetchFridgeHighestConsumptionFromAPI() {
+    try {
+        const response = await fetch('http://127.0.0.1:5000/api/get_fridge_highest_rating_consumption');
+        // const response = await fetch('https://ta21-2023-s2.azurewebsites.net/api/get_fridge_highest_rating_consumption')
         if (!response.ok) {
             throw new Error('API request failed');
         }
@@ -20,6 +80,17 @@ let chart = null; // Declare the chart variable
 
 // Function to update the graph based on user selections
 async function updateGraph() {
+    // Fetch data from the API
+    const jsonData = await FetchAppliancesAPI(); // Await here to get the actual data
+
+    console.log('jsonData:', jsonData);
+
+    if (!Array.isArray(jsonData)) {
+        // Handle the case where jsonData is not an array
+        console.error('Invalid data:', jsonData);
+        return;
+    }
+    
     // Function to initialize or update the chart
     function initializeOrUpdateChart(existingChart, ctx, data) {
         if (existingChart) {
@@ -59,7 +130,8 @@ async function updateGraph() {
     }
 
     // Fetch data from the API
-    const jsonData = await fetchDataFromAPI();
+    // const jsonData = await FetchAppliancesAPI();
+    // const jsonData = await fetchFridgeDataFromAPI();
 
     // Get selected options
     const selectedType = document.getElementById('type').value;
@@ -140,20 +212,18 @@ updateGraph();
 
 // ------------------ calculate section ------------------ 
 
-// function clearErrorMessages(){
-//     // Clear all error messages by setting their innerHTML to an empty string
-//     document.getElementById("errorMessageAllFields").innerHTML = "";
-//     document.getElementById("errorMessageLumens").innerHTML = "";
-//     document.getElementById("errorMessageBulbs").innerHTML = "";
-//     document.getElementById("errorMessageArea").innerHTML = "";
-//   }
+function clearErrorMessages(){
+    // Clear all error messages by setting their innerHTML to an empty string
+    document.getElementById("errorMessageAllFields").innerHTML = "";
+    document.getElementById("errorMessagePower").innerHTML = "";
+    document.getElementById("errorMessageHours").innerHTML = "";
+    document.getElementById("errorMessageCharge").innerHTML = "";
+  }
   
-//   function displayErrorMessage(elementId, message) {
-//     // Display an error message in the specified element
-//     document.getElementById(elementId).innerHTML = message;
-//   }
-
-
+  function displayErrorMessage(elementId, message) {
+    // Display an error message in the specified element
+    document.getElementById(elementId).innerHTML = message;
+  }
 
 function Calculate(){
     let conditionsMet = true;
@@ -162,29 +232,23 @@ function Calculate(){
     let hours = parseFloat(document.getElementById("hours").value);
     let charge = parseFloat(document.getElementById("charge").value);
   
-    // if (!lumens || isNaN(numberOfLightBulb) || areaName === "none" || !areaSize) {
-    //   displayErrorMessage("errorMessageAllFields", "Please fill in all required fields before clicking 'Check'");
-    //   conditionsMet = false;
-    // }
-  
-    // else if (lumens > 3000 || lumens < 1) {
-    //   displayErrorMessage("errorMessageLumens", "Please only enter between 1 to 3000");
-    //   conditionsMet = false;
-    // }
-  
-    // else if (numberOfLightBulb > 20 || numberOfLightBulb < 1) {
-    //   displayErrorMessage("errorMessageBulbs", "Please only enter between 1 to 20");
-    //   conditionsMet = false;
-    // }
-  
-    // else if (areaSize > 3000 || areaSize < 1) {
-    //   displayErrorMessage("errorMessageArea", "Please only enter between 1 to 3000");
-    //   conditionsMet = false;
-    // }
+    
+    if (!power){
+        displayErrorMessage("errorMessagePower", "Please enter the power consumption");
+        conditionsMet = false;
+    } else if (!hours){
+        displayErrorMessage("errorMessageHours", "Please enter the hours of usage");
+        conditionsMet = false;
+    } else if (!charge){
+        displayErrorMessage("errorMessageCharge", "Please provide the amount you are being billed by your energy provider.");
+        conditionsMet = false;
+    } else if (!power || !hours || !charge) {
+        displayErrorMessage("errorMessageAllFields", "Please fill in all required fields before clicking 'Calculate'");
+        conditionsMet = false;}
   
     if (conditionsMet){
-    //   clearErrorMessages();
-      let total = parseFloat((power * hours * charge * 30).toFixed(2));
+      clearErrorMessages();
+      let total = parseFloat((power * hours * charge / 100).toFixed(2));
   
       console.log(total);
   
@@ -194,7 +258,8 @@ function Calculate(){
       <span class="med-text">Your total electricity cost per month would be $${total}.</span><br><br>`      
   
       const hiddenContainer = document.getElementById("hiddenContainer");
-      hiddenContainer.style.backgroundImage = 'url("Front_end/images/bg_lights2.jpg")';
+    //   hiddenContainer.style.backgroundImage = 'url("Front_end/images/bg_lights2.jpg")';
+      hiddenContainer.style.backgroundColor = 'olive';
   
       const textField = document.getElementById("textField");
       textField.innerHTML = resultText;
@@ -203,5 +268,155 @@ function Calculate(){
       const thisDiv = document.getElementById("thisDiv");
       thisDiv.scrollIntoView({ behavior: "smooth" });
   
+      const showText2 = document.getElementById("text2");
+      showText2.style.display = 'flex';
+      
+      compare(total);
     }
   }
+
+function hideTable(){
+    const tableContainer = document.getElementById("table-container");
+    tableContainer.style.display = 'none';
+}
+
+async function showTable(){
+    let appType = document.getElementById("app-type").value;
+    let fridgeType = document.getElementById("fridgeType").value;
+
+    const tableContainer = document.getElementById("table-container");
+    const thisText = document.getElementById("thisText");
+
+    const jsonData = await fetchFridgeConsumptionFromAPI();
+    let filteredData = jsonData.filter(item => item.type === fridgeType); // Filter data based on fridgeType
+    console.log(filteredData);
+
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+
+    // Create an array of keys (column names) to be displayed in the table
+    const keysToDisplay = Object.keys(filteredData[0]).filter(key => key !== "type");
+
+    // Create table header without the "type" column
+    const headerRow = document.createElement("tr");
+    keysToDisplay.forEach(key => {
+        const th = document.createElement("th");
+        th.textContent = key;
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+
+    // Create table body rows without the "type" column
+    filteredData.forEach(item => {
+        const bodyRow = document.createElement("tr");
+        keysToDisplay.forEach(key => {
+        const cell = document.createElement("td");
+        cell.textContent = item[key];
+        bodyRow.appendChild(cell);
+        });
+        tbody.appendChild(bodyRow);
+    });
+
+    // Append the header and body to the table
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    // Append the table to the container
+    tableContainer.innerHTML = ''; // Clear previous content
+    thisText.innerHTML = `Use the table below to assist you!`;
+    tableContainer.appendChild(thisText);
+    tableContainer.appendChild(table);
+
+    // show the table in the webpage
+    tableContainer.style.display = 'grid';
+    tableContainer.style.placeItems = 'center';
+
+    thisText.style.display = 'block';
+
+    table.style.borderCollapse = 'collapse';
+    table.style.border = '2px solid black';
+
+    const cells = table.querySelectorAll('td, th');
+    cells.forEach((cell) => {
+        cell.style.border = '1px solid black'; // Add a black border to all cells
+        cell.style.padding = '8px';
+    });
+
+    // Get all table rows
+    const rows = document.querySelectorAll("tbody tr");
+
+    // Add mouseover and mouseout event listeners to each row
+    rows.forEach((row) => {
+        row.addEventListener("mouseover", function () {
+            // Add a CSS class to highlight the row on mouseover
+            row.classList.add("highlighted-row");
+        });
+
+        row.addEventListener("mouseout", function () {
+            // Remove the CSS class to remove the highlight on mouseout
+            row.classList.remove("highlighted-row");
+        });
+    });
+}
+
+async function compare(result){
+
+    const appType = document.getElementById("app-type").value;
+    const fridgeType = document.getElementById("fridgeType").value;
+    const hours = document.getElementById("hours").value;
+    const charge = document.getElementById("charge").value;
+    
+    const jsonData = await fetchFridgeHighestConsumptionFromAPI();
+
+    // Filter the JSON data based on the selected fridge type
+    const filteredData = jsonData.filter(item => item.type === fridgeType);
+
+    // Extract the energy consumption and star rating from the filtered data
+    const energyConsumptionData = filteredData.map(item => item["Average Energy Consumption (kW)"]);
+    const calcCost = energyConsumptionData * charge * hours / 100;
+    console.log(`charge is ${calcCost}`);
+
+    const savedMoney = document.getElementById('text3');
+    const savings = (result - calcCost).toFixed(2);
+    savedMoney.innerHTML = `You could've saved $${savings} per month by using the highest star rating appliance!`
+    savedMoney.style.padding = '3rem';
+
+    // Create labels for the x-axis
+    const labels = ["You", "Highest Star Rating Appliance"];
+
+    // Create a bar graph using Chart.js
+    const ctx1 = document.getElementById("barChart").getContext("2d");
+
+    // Destroy any existing chart on the canvas
+    if (window.myBar) {
+        window.myBar.destroy();
+    }
+
+    window.myBar = new Chart(ctx1, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Cost per Month',
+                    data: [result, calcCost],
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+                    borderColor: ['rgb(255, 99, 132)', 'rgb(75, 192, 192)'],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: "Total Cost ($)"
+                    }
+                }
+            }
+        }
+    });
+}
